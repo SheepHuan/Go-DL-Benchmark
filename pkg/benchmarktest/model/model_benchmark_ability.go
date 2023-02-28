e691c93b8bc481edb2c9e2c292efa3eacfbcdfb7
SheepHuan <sheephuan@outlook.com>
2023-02-28 21:35:30 +0800

    refactor: 重构代码名称

--- pkg/benchmarktest/model/model_benchmark_ability.go
+++ pkg/benchmarktest/model/model_benchmark_ability.go
@@ -7,19 +7,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type BenchmarkAbility struct {
-	IsSupportHardwareBenchmarkTest bool `json:"IsSupportHardwareBenchmarkTest"`
-	IsSupportModelBenchmarkTest    bool `json:"IsSupportModelBenchmarkTest"`
+type ModelBenchmarkTestAbility struct {
+	//IsSupportHardwareBenchmarkTest bool `json:"IsSupportHardwareBenchmarkTest"`
+	IsSupportModelBenchmarkTest bool `json:"IsSupportModelBenchmarkTest"`
 
 	SupportedFrameworksForStaticAnalysis  []protos.FrameworkType `json:"SupportedFrameworksForStaticAnalysis"`
 	SupportedFrameworksForRuntimeAnalysis []protos.FrameworkType `json:"SupportedFrameworksForRuntimeAnalysis"`
 }
 
-func LoadAndPrintBenchmarkTestAbility(content string) BenchmarkAbility {
-	return BenchmarkAbility{}
-}
-
-func (s *BenchmarkAbility) queryFrameworksSupportStatic(framework protos.FrameworkType) bool {
+func (s *ModelBenchmarkTestAbility) queryFrameworksSupportStatic(framework protos.FrameworkType) bool {
 	found := false
 	for _, supportedFramework := range s.SupportedFrameworksForStaticAnalysis {
 		if framework == supportedFramework {
@@ -31,7 +27,7 @@ func (s *BenchmarkAbility) queryFrameworksSupportStatic(framework protos.Framewo
 	return found
 }
 
-func (s *BenchmarkAbility) queryFrameworksSupportRuntime(framework protos.FrameworkType) bool {
+func (s *ModelBenchmarkTestAbility) queryFrameworksSupportRuntime(framework protos.FrameworkType) bool {
 	found := false
 	for _, supportedFramework := range s.SupportedFrameworksForRuntimeAnalysis {
 		if framework == supportedFramework {
@@ -42,7 +38,7 @@ func (s *BenchmarkAbility) queryFrameworksSupportRuntime(framework protos.Framew
 	return found
 }
 
-func (s *BenchmarkAbility) ModelBenchmarkTest(config *protos.ModelBenchmarkTestArgs, device devices.HardwareDevice) (*protos.ModelAnalysisResult, error) {
+func (s *ModelBenchmarkTestAbility) ModelBenchmarkTest(config *protos.ModelBenchmarkTestArgs, device devices.HardwareDevice) (*protos.ModelAnalysisResult, error) {
 	// 1. 保证设备已连接
 	err := device.Connect()
 	if err != nil {
@@ -52,18 +48,18 @@ func (s *BenchmarkAbility) ModelBenchmarkTest(config *protos.ModelBenchmarkTestA
 	modelTestResult := protos.ModelAnalysisResult{}
 	if s.IsSupportModelBenchmarkTest {
 		if s.queryFrameworksSupportStatic(config.GetFramework()) {
-			if config.Framework == protos.FrameworkType_onnxruntime {
-				staticResult := OnnxruntimeStaticAnalyse(config, device)
-				modelTestResult.StaticResult = staticResult
-			}
+			//if config.Framework == protos.FrameworkType_onnxruntime {
+			//	staticResult := OnnxruntimeStaticAnalyse(config, device)
+			//	modelTestResult.StaticResult = staticResult
+			//}
 		} else {
 			log.Warn(fmt.Sprintf("Don't support static analyse for %s now!", config.Framework))
 		}
 		if s.queryFrameworksSupportRuntime(config.Framework) {
-			//if config.Framework == protos.FrameworkType_onnxruntime {
-			//	runtimeResult := onnxruntimeRuntimeAnalyse(config, device)
-			//	modelTestResult.RuntimeResult = runtimeResult
-			//}
+			if config.Framework == protos.FrameworkType_onnxruntime {
+				runtimeResult := onnxruntimeRuntimeAnalyse(config, device)
+				modelTestResult.RuntimeResult = runtimeResult
+			}
 		} else {
 			log.Warn(fmt.Sprintf("Don't support runtime analyse for %s now!", config.Framework))
 		}
